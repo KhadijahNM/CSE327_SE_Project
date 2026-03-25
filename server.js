eb1e368bd14fb95351272a50f0c030a25d74b6c9
Zubo Islam <m.zubo@yahoo.com>
2026-03-25 18:25:52 +0600

    firebase without credential

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const path = require("path");   
+app.use(express.static(__dirname));  
+
 // ======================
 // MySQL Connection
 // ======================
